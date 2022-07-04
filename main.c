d5adc6c3bd8529a806e705e5379f708757c0254a
Lucidity <2993514952@qq.com>
2022-07-04 16:07:23 +0800

    update

--- main.c
+++ main.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
 int main() {
+    int answer = 42;
+    printf("%d\n", answer);
     printf("Hello, World!\n");
     printf("你好, 世界!\n");
-    printf("你好， 世界！\n");
-    // 这是注释
-
-    /**
-     * 多行注释
-     */
     return 0;
 }
